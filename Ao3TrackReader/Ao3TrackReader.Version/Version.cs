cd806d766e15b4b7672794242f761b785c78c086
Alexis Ryan <the.wench@wenchy.net>
2017-03-22 15:31:00 +1100

    Getting ready for V1.0.0 App Submissions

--- Ao3TrackReader/Ao3TrackReader.Version/Version.cs
+++ Ao3TrackReader/Ao3TrackReader.Version/Version.cs
@@ -7,12 +7,12 @@ namespace Ao3TrackReader.Version
     // Current version of Ao3TrackReader
     static class Version
     {
-        public const int Major = 0;
-        public const int Minor = 9;
-        public const int Build = 9;
+        public const int Major = 1;
+        public const int Minor = 0;
+        public const int Build = 0;
 
-        public const string String = "0.9.9";
-        public const string LongString = "0.9.9.0";
+        public const string String = "1.0.0";
+        public const string LongString = "1.0.0.0";
         public const int Integer = Major * 1000000 + Minor * 1000 + Build;
 
         public const string Copyright = "Copyright © 2017 Alexis Ryan";
