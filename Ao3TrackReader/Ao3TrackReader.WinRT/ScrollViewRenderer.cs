da7b2b274ca4c66f443d62f73b793eaeaa7d0535
Alexis Ryan <the.wench@wenchy.net>
2017-11-26 06:26:54 +1100

    Win81 stuff

--- Ao3TrackReader/Ao3TrackReader.WinRT/ScrollViewRenderer.cs
+++ Ao3TrackReader/Ao3TrackReader.WinRT/ScrollViewRenderer.cs
@@ -1,21 +1,42 @@
-﻿using System;
+﻿/*
+Copyright 2017 Alexis Ryan
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms.Internals;
+#if WINDOWS_UWP
 using Xamarin.Forms.Platform.UWP;
+using XScrollViewRenderer = Xamarin.Forms.Platform.UWP.ScrollViewRenderer;
+#else
+using Xamarin.Forms.Platform.WinRT;
+using XScrollViewRenderer = Xamarin.Forms.Platform.WinRT.ScrollViewRenderer;
+#endif
 using ScrollOrientation = Xamarin.Forms.ScrollOrientation;
 using XScrollView = Xamarin.Forms.ScrollView;
 using Ao3TrackReader.Controls;
 using Windows.UI.Xaml.Controls;
 
-[assembly: ExportRenderer(typeof(ScrollView), typeof(Ao3TrackReader.UWP.ScrollViewRenderer))]
-
-namespace Ao3TrackReader.UWP
+[assembly: ExportRenderer(typeof(ScrollView), typeof(Ao3TrackReader.ScrollViewRenderer))]
+namespace Ao3TrackReader
 {
-    public class ScrollViewRenderer : Xamarin.Forms.Platform.UWP.ScrollViewRenderer
+    public class ScrollViewRenderer : XScrollViewRenderer
     {        
         protected override void OnElementChanged(ElementChangedEventArgs<XScrollView> e)
         {
